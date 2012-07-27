855b6f9a8451c491b6d896f1f6a3b8179955cc2c
Jason Dusek <jason.dusek@gmail.com>
2012-07-27 10:20:24 +0000

    Working configuration loader.

--- Aws/S3P/Configuration.hs
+++ Aws/S3P/Configuration.hs
@@ -9,7 +9,12 @@ import           Control.Exception
 import           Control.Monad
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as Bytes
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           Data.Maybe
+import           Data.Monoid
 import           System.Environment
+import           System.IO
 import           System.IO.Error
 
 import qualified Aws as Aws
@@ -32,30 +37,45 @@ endpoint "ap-northeast-1" = Just "s3-ap-northeast-1.amazonaws.com"
 endpoint "sa-east-1"      = Just "s3-sa-east-1.amazonaws.com"
 endpoint _                = Nothing
 
-[access,secret,region,bucket] =
+variables =
   ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION", "SSSP_BUCKET"]
+
+fromEnv :: IO (Map ByteString ByteString)
+fromEnv  = Map.fromList . catMaybes <$> mapM paired variables
+ where
+  paired k = ((k,) <$>) <$> maybeGetEnv k
 
 maybeGetEnv :: ByteString -> IO (Maybe ByteString)
 maybeGetEnv k = catchJust ((>> Just ()) . guard . isDoesNotExistError)
                           (Just . Bytes.pack <$> (getEnv . Bytes.unpack $ k))
                           (const (return Nothing))
 
-fromEnv = sequence [ maybeGetEnv access
-                   , maybeGetEnv secret
-                   , join . (endpoint <$>) <$> maybeGetEnv region
-                   , maybeGetEnv bucket ]
+fromBytes :: ByteString -> Map ByteString ByteString
+fromBytes bytes = Map.fromList
+  [ (k, v) | Right (k, v) <- parseOnly line <$> Bytes.lines bytes ]
 
-parseLine =
-  optional (string "export") *> skipSpace *> do
-    k <- choice (string <$> [access,secret,region,bucket]) 
-    v <- skipWhile (inClass " :=") *> takeByteString
-    return . (k,) $
-      if k == region then endpoint v else guard (v /= "") >> Just v 
+line :: Parser (ByteString, ByteString)
+line  = optional (string "export") *> skipSpace *> do
+  (,) <$> choice (string <$> variables)
+      <*> (skipWhile (inClass " :=") *> takeTill (inClass "\n\r"))
 
+confFromContext = do
+  env    <- fromEnv
+  pruned <- prune <$> do
+            ready <- hReady stdin
+            if ready
+              then  (`Map.union` env) . fromBytes <$> Bytes.getContents
+              else  return env
+  let bogus = [ k | k <- variables, Map.notMember k pruned ]
+  if bogus /= []
+    then do Bytes.hPutStrLn stderr "Missing or invalid conf parameters:"
+            mapM_ (Bytes.hPutStrLn stderr . mappend "  ") bogus
+            return Nothing
+    else    return (Just pruned)
 
 s3FromEnv :: IO Aws.S3Configuration
 s3FromEnv = do
-  res <- join . (endpoint <$>) <$> maybeGetEnv "AWS_REGION"
+  res <- (endpoint =<<) <$> maybeGetEnv "AWS_REGION"
   pure $ case res of Nothing -> Aws.defaultConfiguration
                      Just s  -> Aws.defaultConfiguration{Aws.s3Endpoint=s}
 
@@ -68,3 +88,9 @@ allFromEnv = do
                                , Aws.logger = Aws.defaultLog Aws.Warning }
   return (conf, s3cfg)
 
+validate :: ByteString -> ByteString -> Maybe ByteString
+validate "AWS_REGION" r = endpoint r
+validate _            s = guard (s /= "") >> Just s
+
+prune :: Map ByteString ByteString -> Map ByteString ByteString
+prune  = Map.mapMaybeWithKey validate
